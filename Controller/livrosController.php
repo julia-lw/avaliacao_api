2f6dab23290188017d82292bddda84748060f0f3
Júlia L. W. <julia_wecolovis@estudante.sesisenai.org.br>
2026-09-10 17:58:26 -0300

    a

--- Controller/livrosController.php
+++ Controller/livrosController.php
@@ -7,24 +7,26 @@ class LivrosController {
     }
 
     public function processarRequisicao($metodo) {
-        $id = isset($_GET['id']) ? $_GET['id'] : null;
         $dados = json_decode(file_get_contents("php://input"), true);
 
         switch ($metodo) {
             case 'GET':
-                if ($id) {
-                    echo json_encode($this->livrosModel->listarPorId($id));
-                } else {
-                    echo json_encode($this->livrosModel->listar());
-                }
+                http_response_code(200);
+                echo json_encode($this->livrosModel->listar(), JSON_UNESCAPED_UNICODE);
                 break;
 
             case 'POST':
                 if ($this->livrosModel->cadastrar($dados)) {
                     http_response_code(201);
                     echo json_encode(["mensagem" => "Livro cadastrado com sucesso!"]);
+                } else {
+                    http_response_code(400);
+                    echo json_encode(["erro" => "Erro ao cadastrar livro."]);
                 }
                 break;
+        }
+    }
+}
 
             case 'PUT':
                 if ($id && $this->livrosModel->atualizar($id, $dados)) {
--- Model/Livros.php
+++ Model/Livros.php
@@ -7,25 +7,25 @@ class Livros {
     }
 
     public function listar() {
-        $query = "SELECT l.id_livros, l.nome_livros, l.genero_livros, l.quantidade_paginas_livros, a.id_autor, a.nome, a.nacionalidade, a.biografia 
+        $query = "SELECT l.id_livros, l.nome_livros, l.genero_livros, l.quantidade_paginas_livros, a.id_autor, a.nome AS autor_nome, a.nacionalidade, a.biografia 
         FROM livros l
         INNER JOIN autor a ON l.Autor_id_autor = a.id_autor";
+
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 
-    $livros = [];
+        $livros = [];
         foreach ($resultados as $linha) {
             $livros[] = [
-                "id_livros" => $linha['id_livros'],
+                "id_livros" => (int)$linha['id_livros'],
                 "nome_livros" => $linha['nome_livros'],
                 "genero_livros" => $linha['genero_livros'],
-                "quantidade_paginas" => $linha['quantidade_paginas_livros'],
+                "quantidade_paginas_livros" => (int)$linha['quantidade_paginas_livros'],
                 "autor" => [
-                    "id_autor" => $linha['id_autor'],
-                    "nome" => $linha['nome'],
-                    "nacionalidade" => $linha['nacionalidade']
+                    "id_autor" => (int)$linha['id_autor'],
+                    "nome" => $linha['autor_nome'],
+                    "nacionalidade" => $linha['nacionalidade'],
                     "biografia" => $linha['biografia']
                 ]
             ];
@@ -34,46 +34,14 @@ class Livros {
     }
 
     public function cadastrar($dados) {
-        $query = "INSERT INTO livros (nome_livros, genero_livros, quantidade_paginas_livros) VALUES (:nome_livros, :genero_livros, :quantidade_paginas_livros)";
+        $query = "INSERT INTO livros (nome_livros, genero_livros, quantidade_paginas_livros, Autor_id_autor) 
+                  VALUES (:nome, :genero, :paginas, :autor_id)";
+                  
         $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':nome_livros', $dados['nome_livros']);
-        $stmt->bindParam(':genero_livros', $dados['genero_livros']);
-        $stmt->bindParam(':quantidade_paginas_livros', $dados['quantidade_paginas_livros']);
-
-        return $stmt->execute();
-    }
-
-    public function atualizar($id, $dados) {
-        $query = "UPDATE livros SET nome = :nome, genero = :genero, quantidade_paginas = :quantidade_paginas WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':nome_livros', $dados['nome_livros']);
-        $stmt->bindParam(':genero_livros', $dados['genero_livros']);
-        $stmt->bindParam(':quantidade_paginas_livros', $dados['quantidade_paginas_livros']);
-        $stmt->bindParam(':id', $id);
-
-        return $stmt->execute();
-    }
-
-    public function atualizarParcial($id, $dados) {
-        $campos = [];
-        foreach ($dados as $chave => $valor) {
-            $campos[] = "$chave = :$chave";
-        }
-        $query = "UPDATE livros SET " . implode(', ', $campos) . " WHERE id = :id";
-        
-        $stmt = $this->conn->prepare($query);
-        foreach ($dados as $chave => $valor) {
-            $stmt->bindValue(":$chave", $valor);
-        }
-        $stmt->bindValue(':id', $id);
-
-        return $stmt->execute();
-    }
-
-    public function deletar($id) {
-        $query = "DELETE FROM livros WHERE id = :id";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nome', $dados['nome_livros']);
+        $stmt->bindParam(':genero', $dados['genero_livros']);
+        $stmt->bindParam(':paginas', $dados['quantidade_paginas_livros']);
+        $stmt->bindParam(':autor_id', $dados['Autor_id_autor']);
 
         return $stmt->execute();
     }
